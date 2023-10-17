d3e8ef5855be52efb9e7194c5a175a541fa71849
Owen Zuercher <ozuercher2013@gmail.com>
2023-10-17 18:41:42 -0500

    updated JS, CSS, and HTML updated code

--- main/assets/javascript/Script.js
+++ main/assets/javascript/Script.js
@@ -1,82 +1,169 @@
-/*
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
-*/
-
-
-// TIMER JUST PAUSES WHEN VIEWING SCORES
-// ( ?? SHOULD PAUSE OR RESET ?? )
-
-//this lovely mess (CLEANUP)
-var checkHighscores = document.getElementById("highscores");
 var checkStart = document.getElementById("start");
 var timeEl = document.getElementById("timer");
+var checkHighscores = document.getElementById("highscores");
+var submitButton = document.getElementById("submit");
+var backButton = document.getElementById("back-to-quiz");
+
+var quizContainer = document.querySelector(".quiz-container");
 var startContainer = document.querySelector(".start-container");
 var questionContainer = document.querySelector(".question-container");
-var highScoresContainer = document.querySelector(".highscores-container");
-var backToQuizButton = document.getElementById("back-to-quiz");
-var quizContainer = document.querySelector(".quiz-container");
+var endContainer = document.querySelector(".end-container");
+var highscoresContainer = document.querySelector(".highscores-container");
 
 var secondsLeft = 75; //initial time to start with
 var timerInterval;
 
-//function to start the quiz
+const questionsArray = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts"
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within ____.",
+    answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctAnswer: "parentheses"
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____.",
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: "all of the above"
+  },
+  {
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    answers: ["commas", "curly brackets", "quotes", "parentheses"],
+    correctAnswer: "quotes"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    correctAnswer: "console.log"
+  }
+];
+
+var currentQuestionIndex = 0;
+
 function startQuiz(){
-  checkStart.addEventListener("click", function(){
-    timeEl.textContent = "Time: " + secondsLeft; //BUG FIX - timer visuals #1
+  checkStart.addEventListener("click", function (){
+    secondsLeft = 75;
+    timeEl.textContent = "Time: " + secondsLeft;
     startContainer.style.display = "none";
     questionContainer.style.display = "block";
-    startTimer(); //starts timer
+    startTimer();
+    askQuestion();
   });
 
-  //function for timer, determines score and game over
   function startTimer(){
-    timerInterval = setInterval(function(){
+    timerInterval = setInterval(function (){
       secondsLeft--;
       timeEl.textContent = "Time: " + secondsLeft;
-
-      if(secondsLeft <= 0){ //ends game when time reaches zero
+      if(secondsLeft <= 0){
         clearInterval(timerInterval);
         gameOver();
       }
     }, 1000);
   }
 
-  //display the scores screen
+  function askQuestion(){
+    if (currentQuestionIndex < questionsArray.length){
+      var currentQuestion = questionsArray[currentQuestionIndex];
+      var questionElement = document.getElementById("question");
+      var answerA = document.getElementById("answerA");
+      var answerB = document.getElementById("answerB");
+      var answerC = document.getElementById("answerC");
+      var answerD = document.getElementById("answerD");
+  
+      questionElement.textContent = currentQuestion.question;
+      answerA.textContent = "1. " + currentQuestion.answers[0];
+      answerB.textContent = "2. " + currentQuestion.answers[1];
+      answerC.textContent = "3. " + currentQuestion.answers[2];
+      answerD.textContent = "4. " + currentQuestion.answers[3];
+  
+      answerA.addEventListener("click", function(){
+        checkAnswer(currentQuestion.answers[0], currentQuestion.correctAnswer);
+      });
+      answerB.addEventListener("click", function(){
+        checkAnswer(currentQuestion.answers[1], currentQuestion.correctAnswer);
+      });
+      answerC.addEventListener("click", function(){
+        checkAnswer(currentQuestion.answers[2], currentQuestion.correctAnswer);
+      });
+      answerD.addEventListener("click", function(){
+        checkAnswer(currentQuestion.answers[3], currentQuestion.correctAnswer);
+      });
+    } else {
+      gameOver();
+    }
+  }
+
+  function checkAnswer(selectedAnswer, correctAnswer){
+    if (selectedAnswer !== correctAnswer) {
+      secondsLeft -= 10;
+    }
+  
+    if (currentQuestionIndex < questionsArray.length - 1){
+      currentQuestionIndex++;
+      askQuestion();
+    } else {
+      currentQuestionIndex = 0;
+      gameOver();
+    }
+  }
+  
+  function gameOver(){
+    clearInterval(timerInterval);
+    questionContainer.style.display = "none";
+    endContainer.style.display = "block";
+    var finalScoreElement = document.querySelector("#final-score");
+    finalScoreElement.textContent = "Your final score is: " + secondsLeft;
+  }
+
   checkHighscores.addEventListener("click", function(){
-    clearInterval(timerInterval); //BUG FIX - stops timer
+    clearInterval(timerInterval);
     quizContainer.style.display = "none";
-    highScoresContainer.style.display = "block";
+    endContainer.style.display = "none";
+    highscoresContainer.style.display = "block";
+    renderLastScore();
+  });
+  
+  function saveLastScore(){
+    var studentGrade = {
+      student: student.value,
+      grade: grade.value,
+      comment: comment.value.trim(),
+    };
+    localStorage.setItem('studentGrade', JSON.stringify(studentGrade));
+  }
+  
+  function renderLastScore(){
+    var lastScore = JSON.parse(localStorage.getItem('studentGrade'));
+    if (lastScore !== null){
+      document.getElementById('saved-name').innerHTML = lastScore.student;
+      document.getElementById('saved-comment').innerHTML = lastScore.comment;
+    }
+  }
+  
+  submitButton.addEventListener('click', function (event){
+    event.preventDefault();
+    timeEl.textContent = "Time: 0";
+    questionContainer.style.display = "none";
+    endContainer.style.display = "none";
+    startContainer.style.display = "none";
+    quizContainer.style.display = "none";
+    highscoresContainer.style.display = "block";
+    saveLastScore();
+    renderLastScore();
   });
 
-  //display the starting screen
-  backToQuizButton.addEventListener("click", function(){
-    timeEl.textContent = "Time: 0"; //BUG FIX - timer visuals #2
-    highScoresContainer.style.display = "none";
+  backButton.addEventListener("click", function (){
+    timeEl.textContent = "Time: 0";
+    highscoresContainer.style.display = "none";
     questionContainer.style.display = "none";
+    endContainer.style.display = "none";
     startContainer.style.display = "block";
     quizContainer.style.display = "block";
+    currentQuestionIndex = 0;
   });
-
-  //funtion for updating to each question
-  function askQuestion(){}
-
-  //function to check for correct answers (ADD THE TIME DEDUCTION FOR WRONG ANSWERS)
-  function checkAnswer(){}
-
-  function gameOver(){
-    quizContainer.style.display = "none";
-    highScoresContainer.style.display = "block";
-  }
 }
 
 startQuiz();
